d7956f50cc1956454686dcfa7fe61c9a7dc75274
yahyachan <danihao123@outlook.com>
2023-05-31 18:37:18 +0800

    make type inference stratified

--- lib/reconstruction.ml
+++ lib/reconstruction.ml
@@ -8,11 +8,11 @@ let rec type_substitute env = function
   | TFun (s, t) -> TFun (type_substitute env s, type_substitute env t)
   | t -> t
 
-let instantiate stream (PolyType (vars, t)) =
+let instantiate level uf (PolyType (vars, t)) =
   match vars with
   | [] -> t
   | _ ->
-    let assoc = List.map (fun u -> u, Stream.next stream) vars in
+    let assoc = List.map (fun u -> u, Utils.Unionfind.new_var uf (fun x -> `TVar (level, x))) vars in
     let env = Typevar_env.of_seq @@ List.to_seq assoc in 
     type_substitute env t
 
@@ -35,10 +35,12 @@ let rec unify uf a b =
     unify uf s1 s2; unify uf t1 t2
   | TVar x, TVar y -> begin
     match get uf x, get uf y with
-    | TVar s, TVar t ->
+    | `TVar (ls, s), `TVar (lt, t) ->
+      let minorant = if ls < lt then s else t in
       merge_set uf s t;
-      set uf s @@ TVar (Int.min s t)
-    | s, t -> unify uf s t
+      set uf s @@ `TVar (Int.min ls lt, minorant)
+    | `Cont s, `Cont t -> unify uf s t
+    | `Cont s, `TVar (_, v) | `TVar (_, v), `Cont s -> unify uf s (TVar v)
   end
   | TVar x, t | t, TVar x ->
     if check_occur uf x t then raise @@ Type_recursion (x, t) else try_set uf x t
@@ -46,61 +48,57 @@ let rec unify uf a b =
 and try_set uf pos t =
   let open Utils.Unionfind in
   match get uf pos with
-  | TVar _ -> set uf pos t
-  | u -> unify uf u t
+  | `TVar _ -> set uf pos @@ `Cont t
+  | `Cont u -> unify uf u t
 
 let rec travel uf tbl lp = function
   | TFun (s, t) -> TFun (travel uf tbl lp s, travel uf tbl lp t)
   | TVar x -> begin
     match Utils.Unionfind.get uf x with
-    | TVar s -> if s >= lp then Hashtbl.replace tbl s () else (); TVar s
-    | t -> travel uf tbl lp t
+    | `TVar (s, v) -> if s >= lp then Hashtbl.replace tbl v () else (); TVar v
+    | `Cont t -> travel uf tbl lp t
   end
   | prim -> prim
 
-let solve lp size t constraints =
-  let open Utils.Unionfind in
-  let uf = init size (fun x -> TVar x) in
+let solve lp uf t constraints =
   List.iter (fun (a, b) -> unify uf a b) constraints;
   let tbl = Hashtbl.create 10 in
   let res_typ = travel uf tbl lp t in
   PolyType (List.of_seq @@ Hashtbl.to_seq_keys tbl, res_typ)
 
-let rec reconstruct lp stream env tm =
-  let t, con = to_constriants stream env tm in
-  let size = Stream.next stream in
-  solve lp size t con
-and to_constriants stream env tm = 
+let rec reconstruct lp uf env tm =
+  let t, con = to_constriants lp uf env tm in
+  solve lp uf t con
+and to_constriants lp uf env tm = 
   match tm.e with
   | Int _ -> TInt, []
   | Bool _ -> TBool, []
-  | Var s -> instantiate stream @@ Env.find s env, []
+  | Var s -> instantiate lp uf @@ Env.find s env, []
   | Binop (op, l, r) ->
     let inp_type, out_type = side_typeof op in
-    let l, l_con = to_constriants stream env l in
-    let r, r_con = to_constriants stream env r in
+    let l, l_con = to_constriants lp uf env l in
+    let r, r_con = to_constriants lp uf env r in
     out_type, (l, inp_type) :: (r, inp_type) :: List.append l_con r_con
   | If (b, l, r) ->
-    let b, b_con = to_constriants stream env b in
-    let l, l_con = to_constriants stream env l in
-    let r, r_con = to_constriants stream env r in
+    let b, b_con = to_constriants lp uf env b in
+    let l, l_con = to_constriants lp uf env l in
+    let r, r_con = to_constriants lp uf env r in
     l, (b, TBool) :: (l, r) :: (b_con @ l_con @ r_con)
   | Fun (x, t) ->
-    let nv = TVar (Stream.next stream) in
-    let res, con = to_constriants stream (Env.add x (PolyType ([], nv)) env) t in
+    let nv = TVar (Utils.Unionfind.new_var uf (fun x -> `TVar (lp, x))) in
+    let res, con = to_constriants lp uf (Env.add x (PolyType ([], nv)) env) t in
     TFun (nv, res), con
   | Let (x, s, t) ->
-    let nlp = Stream.next stream in
-    let x_typ = reconstruct nlp stream env s in
-    to_constriants stream (Env.add x x_typ env) t
+    let x_typ = reconstruct (lp + 1) uf env s in
+    to_constriants lp uf (Env.add x x_typ env) t
   | Apply (s, t) ->
-    let res = TVar (Stream.next stream) in
-    let t1, c1 = to_constriants stream env s in 
-    let t2, c2 = to_constriants stream env t in
+    let res = TVar (Utils.Unionfind.new_var uf (fun x -> `TVar (lp, x))) in
+    let t1, c1 = to_constriants lp uf env s in 
+    let t2, c2 = to_constriants lp uf env t in
     res, (t1, TFun (t2, res)) :: (c1 @ c2)
   | Fix (x, t) ->
-    let self_typ = TVar (Stream.next stream) in
-    let res, con = to_constriants stream (Env.add x (PolyType ([], self_typ)) env) t in
+    let self_typ = TVar (Utils.Unionfind.new_var uf (fun x -> `TVar (lp, x))) in
+    let res, con = to_constriants lp uf (Env.add x (PolyType ([], self_typ)) env) t in
     res, (res, self_typ) :: con
 
-let reconstruct_toplevel env t = reconstruct 0 (new_stream ()) env t
+let reconstruct_toplevel env t = reconstruct 0 (Utils.Unionfind.init 1 (fun x -> `TVar (0,x))) env t
